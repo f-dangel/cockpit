[FIX] Corrected alpha computation.
I incorreclty assumed the first evaluation to always be at 1, but this would require rescaling of the derivative.

Instead, the observation now happen at 0 and ||Theta_i - Theta_i+1|| = alpha ||\nabla L||, where the equality is only true for SGD.

backboard/utils/cockpit_utils.py
@@ -1,3 +1,5 @@
+"""Utility functions for the Cockpit. """
+
 import numpy as np
 import torch
 
@@ -15,16 +17,34 @@ def _layerwise_dot_product(x_s, y_s):
     return [torch.sum(x * y).item() for x, y in zip(x_s, y_s)]
 
 
-def _fit_quadratic(fs, dfs, fs_var, dfs_var):
-    # We always assume that the observations will be at 0 and 1
-    # Observation matrix (f(0), then f(1), then f'(0) and f'(1))
+def _fit_quadratic(t, fs, dfs, fs_var, dfs_var):
+    # The first observation is always at 0, the second at t
+    # Observation matrix (f(0), then f(t), then f'(0) and f'(t))
     # The matrix shows how we observe the parameters of the quadratic
     # i.e. for f(0) we see (0)**2 *w1 + (0)*w2 + w3
-    # and for f'(1) we see 2*(1)*w1 + w2
-    Phi = np.array([[1, 0, 0], [1, 1, 1], [0, 1, 0], [0, 1, 2]]).T
+    # and for f'(t) we see 2*(t)*w1 + w2
+
+    Phi = np.array([[1, 0, 0], [1, t, t ** 2], [0, 1, 0], [0, 1, 2 * t]]).T
 
     # Covariance matrix
     lambda_inv = np.linalg.inv(np.diag(fs_var + dfs_var))
 
     # Maximum Likelihood Estimation
     return np.linalg.inv(Phi @ lambda_inv @ Phi.T) @ Phi @ lambda_inv @ (fs + dfs)
+
+
+def _get_alpha(mu, t):
+    """Compute the local step size alpha.
+
+    It will be expressed in terms of the local quadratic fit. A local step size
+    of 1, is equal to `stepping on the other side of the quadratic`, while a 
+    step size of 0 means `stepping to the minimum`.
+
+    Args:
+        mu (list): Parameters of the quadratic fit.
+    """
+    # get alpha_bar (the step size that is "the other side")
+    alpha_bar = -mu[1] / mu[2]
+
+    # scale everything on a standard quadratic (i.e. step of 0 = minimum)
+    return (2 * t) / alpha_bar - 1