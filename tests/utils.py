0825cf15a4600f138d107e4d87e26897bffc2cee
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-08-26 13:46:56 +0200

    Filter print messages from downloading datasets

--- tests/utils.py
+++ tests/utils.py
@@ -3,11 +3,23 @@
 import subprocess
 
 
-def run_command(cmd):
-    """Run a command line command and print stdout. Print stderr if command fails."""
-    result = subprocess.run(cmd, capture_output=True)
+def run_command(cmd, filter_download_progress=True):
+    """Run a command line command and print stdout. Print stderr if command fails.
 
-    print(result.stdout.decode("utf-8"))
+    Remove print statements from download progress (less than 7 chars, contains "%").
+    """
+    result = subprocess.run(cmd, capture_output=True)
+    stdout = result.stdout.decode("utf-8").splitlines()
+
+    def is_download(line):
+        """Return whether a string is from a download in progress."""
+        return "%" in line and len(line) < 7
+
+    if filter_download_progress:
+        stdout = [line for line in stdout if not is_download(line)]
+
+    stdout = "\n".join(stdout)
+    print(stdout)
 
     if result.returncode != 0:
         print(result.stderr.decode("utf-8"))
