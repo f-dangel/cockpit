bff81acb3fb5cca9fbbf067289a71fa2276acac8
Frank Schneider <frank.stefan.schneider@gmail.com>
2020-09-11 15:38:38 +0200

    [REF] Remove the not-implemented airline plotter

--- backboard/__init__.py
+++ backboard/__init__.py
@@ -1,8 +1,7 @@
-"""Init for the three main parts of the Cockpit."""
+"""Init for the two main parts of the Cockpit."""
 
 
-from backboard.airline_plotter import AirlinePlotter
 from backboard.cockpit import Cockpit
 from backboard.cockpit_plotter import CockpitPlotter
 
-__all__ = ["AirlinePlotter", "Cockpit", "CockpitPlotter"]
+__all__ = ["Cockpit", "CockpitPlotter"]
