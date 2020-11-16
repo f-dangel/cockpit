48f787fd5ad356a398130a15adc136efacc38309
Frank Schneider <frank.stefan.schneider@gmail.com>
2020-11-16 13:42:17 +0100

    [TEX] Add path length to distance gauge

--- backboard/instruments/distance_gauge.py
+++ backboard/instruments/distance_gauge.py
@@ -32,6 +32,9 @@ def distance_gauge(self, fig, gridspec):
     self.tracking_data["d2init_all"] = self.tracking_data.d2init.map(
         lambda x: _root_sum_of_squares(x) if type(x) == list else x
     )
+    self.tracking_data["update_size_all"] = self.tracking_data.update_size.map(
+        lambda x: _root_sum_of_squares(x) if type(x) == list else x
+    ).cumsum()
 
     plot_args = {
         "x": "iteration",
@@ -51,3 +54,20 @@ def distance_gauge(self, fig, gridspec):
     }
     ax = fig.add_subplot(gridspec)
     create_basic_plot(**plot_args, ax=ax)
+
+    ax2 = ax.twinx()
+    plot_args = {
+        "x": "iteration",
+        "y": "update_size_all",
+        "data": self.tracking_data,
+        "y_scale": "linear",
+        "x_scale": "symlog" if self.show_log_iter else "linear",
+        "cmap": self.cmap,
+        "EMA": "y",
+        "EMA_alpha": self.EMA_alpha,
+        "EMA_cmap": self.cmap2,
+        "xlim": "tight",
+        "ylim": None,
+        "marker": ",",
+    }
+    create_basic_plot(**plot_args, ax=ax2)
