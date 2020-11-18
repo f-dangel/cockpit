d86c38443aee089226717e239c51ff51a3431e1f
Frank Schneider <frank.stefan.schneider@gmail.com>
2020-11-18 13:52:23 +0100

    [FMT] Update Description

--- setup.py
+++ setup.py
@@ -23,19 +23,22 @@ AUTHORS = "F. Schneider, F. Dangel"
 NAME = "cockpit"
 PACKAGES = find_packages()
 
-DESCRIPTION = r"""A Cockpit for Training Neural Networks."""
-LONG_DESCR = r"""Training neural networks using gradient-based methods is rather
-an art than a science due to the lack of theoretical guarantees. It requires
-intuition/experience gained from hours of hyperparameter tuning. Commonly, a
-static or dynamic schedule is defined for all hyperparameters during training,
-looping over a grid. Based on the learning curves of the test/train objective
-and classification accuracy it is then decided, which setting is most
-promising. While this procedure can be highly automated, it suffers from a poor
-gain of insight in the optimization procedure. Almost never, quantities are
-used during training to adapt the optimization strategy. We use BackPACK to
-monitor quantities during training with the goal of both informing the machine
-learning engineer about the state of training, and to identify useful rules for
-when and how to change the optimization strategy if training stagnates."""
+DESCRIPTION = r"""A Practical Debugging Tool for Training Deep Neural Networks."""
+LONG_DESCR = r"""When engineers train deep learning models, they are very much
+“flying blind”. Commonly used approaches for real-time training diagnostics,
+such as monitoring the train/test loss, are limited. Assessing a network's
+training process solely through these performance indicators is akin to
+debugging software without access to internal states through a debugger. To
+address this, we present Cockpit, a collection of instruments that enable a
+closer look into the inner workings of a learning machine, and a more
+informative and meaningful status report for practitioners. It facilitates the 
+identification of learning phases and failure modes, like ill-chosen
+hyperparameters. These instruments leverage novel higher-order information about
+the gradient distribution and curvature, which has only recently become
+efficiently accessible. We believe that such a debugging tool, which we
+open-source for PyTorch, represents an important step to improve troubleshooting
+the training process, reveal new insights, and help develop novel methods and
+heuristics."""
 
 VERSIONFILE = "cockpit/_version.py"
 VERSION = _extract_version()
@@ -54,7 +57,6 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCR,
     long_description_content_type="text/markdown",
-    # install_requires=requirements,
     url=URL,
     license=LICENSE,
     packages=PACKAGES,
