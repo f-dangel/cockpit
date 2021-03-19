e7fc5f9a03addf8e77d7495924c8d6d67864be33
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2021-03-19 11:08:48 +0100

    [TST] Check numerical correctness of CABS

---
+++ tests/test_quantities/test_cabs.py
@@ -0,0 +1,76 @@
+"""Compare ``CABS`` quantity with ``torch.autograd``."""
+
+import pytest
+
+from cockpit.context import get_individual_losses
+from cockpit.quantities import CABS
+from cockpit.utils.schedules import linear
+from tests.test_quantities.settings import (
+    INDEPENDENT_RUNS,
+    INDEPENDENT_RUNS_IDS,
+    PROBLEMS,
+    PROBLEMS_IDS,
+)
+from tests.test_quantities.utils import autograd_diagonal_variance, get_compare_fn
+
+
+class AutogradCABS(CABS):
+    """``torch.autograd`` implementation of ``CABS``."""
+
+    def extensions(self, global_step):
+        """Return list of BackPACK extensions required for the computation.
+
+        Args:
+            global_step (int): The current iteration number.
+
+        Returns:
+            list: (Potentially empty) list with required BackPACK quantities.
+        """
+        return []
+
+    def create_graph(self, global_step):
+        """Return whether access to the forward pass computation graph is needed.
+
+        Args:
+            global_step (int): The current iteration number.
+
+        Returns:
+            bool: ``True`` if the computation graph shall not be deleted,
+                else ``False``.
+        """
+        return self.should_compute(global_step)
+
+    def _compute(self, global_step, params, batch_loss):
+        """Evaluate the CABS criterion.
+
+        Args:
+            global_step (int): The current iteration number.
+            params ([torch.Tensor]): List of torch.Tensors holding the network's
+                parameters.
+            batch_loss (torch.Tensor): Mini-batch loss from current step.
+        """
+        losses = get_individual_losses(global_step)
+        batch_axis = 0
+        trace_variance = autograd_diagonal_variance(
+            losses, params, concat=True, unbiased=False
+        ).sum(batch_axis)
+        lr = self.get_lr()
+
+        return (lr * trace_variance / batch_loss).item()
+
+
+@pytest.mark.parametrize("problem", PROBLEMS, ids=PROBLEMS_IDS)
+@pytest.mark.parametrize("independent_runs", INDEPENDENT_RUNS, ids=INDEPENDENT_RUNS_IDS)
+def test_early_stopping(problem, independent_runs):
+    """Compare BackPACK and ``torch.autograd`` implementation of CABS.
+
+    Args:
+        problem (tests.utils.Problem): Settings for train loop.
+        independent_runs (bool): Whether to use to separate runs to compute the
+            output of every quantity.
+    """
+    interval, offset = 1, 2
+    schedule = linear(interval, offset=offset)
+
+    compare_fn = get_compare_fn(independent_runs)
+    compare_fn(problem, (CABS, AutogradCABS), schedule)
