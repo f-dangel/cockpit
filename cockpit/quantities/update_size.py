26b4490c79e112776fb78f16ac5dbc17fbc6df63
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2021-03-23 13:13:47 +0100

    [DOC] Add documentation to UpdateSize quantity

--- cockpit/quantities/update_size.py
+++ cockpit/quantities/update_size.py
@@ -4,6 +4,8 @@ from cockpit.quantities.quantity import TwoStepQuantity
 
 
 class UpdateSize(TwoStepQuantity):
+    """Quantity for tracking parameter update sizes."""
+
     def __init__(self, track_schedule, verbose=False):
         """Initialize the Quantity by storing the track interval.
 
@@ -32,20 +34,65 @@ class UpdateSize(TwoStepQuantity):
         return []
 
     def is_start(self, global_step):
+        """Return whether current iteration is start point.
+
+        Args:
+            global_step (int): The current iteration number.
+
+        Returns:
+            bool: Whether ``global_step`` is a start point.
+        """
         return self._track_schedule(global_step)
 
     def is_end(self, global_step):
+        """Return whether current iteration is end point.
+
+        Args:
+            global_step (int): The current iteration number.
+
+        Returns:
+            bool: Whether ``global_step`` is an end point.
+        """
         return self._track_schedule(global_step - self._save_shift)
 
     def _compute_start(self, global_step, params, batch_loss):
+        """Perform computations at start point (store current parameter values).
+
+        Modifies ``self._cache``.
+
+        Args:
+            global_step (int): The current iteration number.
+            params ([torch.Tensor]): List of torch.Tensors holding the network's
+                parameters.
+            batch_loss (torch.Tensor): Mini-batch loss from current step.
+        """
         params_copy = [p.data.clone().detach() for p in params]
 
         def block_fn(step):
+            """Block deletion of parameters for current and next iteration.
+
+            Args:
+                step (int): Iteration number.
+
+            Returns:
+                bool: Whether deletion is blocked in the specified iteration
+            """
             return 0 <= step - global_step <= self._save_shift
 
         self.save_to_cache(global_step, self._cache_key, params_copy, block_fn)
 
     def _compute_end(self, global_step, params, batch_loss):
+        """Compute and return update size.
+
+        Args:
+            global_step (int): The current iteration number.
+            params ([torch.Tensor]): List of torch.Tensors holding the network's
+                parameters.
+            batch_loss (torch.Tensor): Mini-batch loss from current step.
+
+        Returns:
+            [float]: Layer-wise L2-norms of parameter updates.
+        """
         params_start = self.load_from_cache(
             global_step - self._save_shift, self._cache_key
         )
